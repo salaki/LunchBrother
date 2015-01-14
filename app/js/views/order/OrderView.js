d403535540dd82928fe04a4ce082cc1648291b35
vampire2014 <zx1989@gwu.edu>
2015-01-14 13:34:11 +0000

    completed

--- app/js/views/order/OrderView.js
+++ app/js/views/order/OrderView.js
@@ -1,41 +1,92 @@
 define([
-  'jQuery',
-  'Underscore',
-  'Backbone',
-  'models/owner/OrderModel',
-  'text!templates/order/orderTemplate.html'
-], function($, _, Backbone, OrderModel, orderTemplate){
-  
-  var FooterView = Backbone.View.extend({
-    el: $("#page"),
-
-    initialize: function() {
-
-      var that = this;
-      var options = {query: 'thomasdavis'}
-     
-
-      var onDataHandler = function(collection) {
-          that.render();
-      }
-
-      this.model = new OrderModel(options);
-      this.model.fetch({ success : onDataHandler, dataType: "jsonp"});
-
+  'models/dish/DishCollection',
+  'models/order/OrderModel',
+  'models/order/PaymentModel',
+  'views/home/DishCollectionView',
+  'text!templates/home/statsTemplate.html',
+  'text!templates/order/orderTemplate.html',
+  'stripe',
+  'libs/semantic/checkbox.min'
+], function(DishCollection, OrderModel, PaymentModel, DishCollectionView, statsTemplate, orderTemplate, Stripe) {
+  Stripe.setPublishableKey('pk_test_EMIAzyTdHHJaFEnWTNchuOTZ');
+  var OrderView = Parse.View.extend({
+    tagName: "div",
+    attributes: {
+      class: 'column'
     },
 
-    render: function(){
+    template: _.template(orderTemplate),
 
-      var data = {
-        owner: this.model.toJSON(),
-        _: _ 
-      };
+    events: {
+      'click #orderBtn': 'orderSubmit'
+    },
 
-      var compiledTemplate = _.template( orderTemplate, data );
-      this.$el.html(compiledTemplate);
-    }
+    initialize: function() {
+      _.bindAll(this, 'render', 'stripeResponseHandler', 'getAmount', 'orderSubmit');
+      Stripe.setPublishableKey('pk_test_EMIAzyTdHHJaFEnWTNchuOTZ');
+      Parse.Events.on('final:update', this.getAmount, this);
+    },
 
+    render: function() {
+      $(this.el).html(this.template());
+      this.$('.ui.checkbox').checkbox();
+      this.$('select.dropdown').dropdown();
+      this.$('form').submit(this.orderSubmit);
+      return this;
+    },
+
+    stripeResponseHandler: function(status, response) {
+      var $form = $('#paymentForm');
+      var self = this;
+      
+      if (response.error) {
+        // Show the errors on the form
+        $form.find('.payment-errors').text(response.error.message);
+        $('#orderBtn').prop('disabled', false);
+      }
+      else { // No errors, submit the form.
+        // Get the token from the response:
+        var token = response.id;
+        var paymentDetails = new PaymentModel();
+        var name = $form.find('input[name="first_name"]').text();
+        var stripepayment = self.model.totalCharge*100;
+        
+        //name: get card name from input 
+        paymentDetails.set('name', name);
+        paymentDetails.set('stripeToken', token);
+        paymentDetails.save(null, {
+          success: function(paymentDetails) {
+            Parse.Cloud.run('pay', {
+              amount:stripepayment,
+              stripeToken: token
+            }, {
+              success: function(amount) {
+                //save
+                alert('Success: ' + amount);
+              },
+              error: function(error) {
+                console.log(error);
+              }
+            });
+          },
+          error: function(payment, error) {
+            alert('Failed to create new object, with error code: ' + error.message);
+          }
+        });
+      }
+    },
+
+    getAmount: function() {
+      var amount = this.final.get();
+      return amount;
+    },
+
+
+    orderSubmit: function(e) {
+      var $form = this.$('form');
+      $form.find('#orderBtn').prop('disabled', true);
+      Stripe.card.createToken($form, this.stripeResponseHandler);
+    },
   });
-
-  return FooterView;
+  return OrderView;
 });
