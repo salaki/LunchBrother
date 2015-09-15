a38b1ed75ff2646b7db47d8aaeada39a01379b64
Ling Hung <ling@xappmedia.com>
2015-09-15 15:04:45 -0400

    MPD-55 Todo for Lian Part II

--- app/js/views/manage/ManageRestaurantsView.js
+++ app/js/views/manage/ManageRestaurantsView.js
@@ -1,12 +1,14 @@
 define([
-  'text!templates/manage/manageRestaurantsTemplate.html'
-], function (manageRestaurantsTemplate) {
+    'models/Restaurant',
+    'models/dish/DishModel',
+    'text!templates/manage/manageRestaurantsTemplate.html',
+    'text!templates/manage/manageRestaurantDishListTemplate.html'
+], function (RestaurantModel, DishModel, manageRestaurantsTemplate, manageRestaurantDishListTemplate) {
 
     var ManageRestaurantsView = Parse.View.extend({
         el: $("#page"),
 
         events: {
-            //TODO@Lian: Add a button event here to click to a method which directs the user to another page
             "click .toNewRestaurant": "toNewRestaurantPageClick"
         },
 
@@ -15,10 +17,32 @@ define([
         },
 
         template: _.template(manageRestaurantsTemplate),
+        dishListTemplate: _.template(manageRestaurantDishListTemplate),
 
         render: function () {
-            this.$el.html(this.template());
-            return this;
+            var self = this;
+            var restaurantQuery = new Parse.Query(RestaurantModel);
+            restaurantQuery.find({
+                success: function(restaurants) {
+                    self.$el.html(self.template({restaurants: restaurants}));
+                    $(".manage-restaurant-selection").dropdown({
+                        onChange: function (restaurantId) {
+                            self.refreshDishList(restaurantId);
+                        }
+                    });
+                },
+                error: function(error) {
+                    alert("Find restaurants failed! Reason: " + error.message);
+                }
+            });
+        },
+
+        refreshDishList: function(restaurantId) {
+            console.log(restaurantId);
+            //TODO@Lian - Step 1. Use Parse Query command to find the dishes belong to restaurantId (Use DishModel)
+            //TODO@Lian - Step 2. Once you find the dishes, pass the dishes object to the dishListTemplate like:
+            //TODO@Lian -         self.$("#dishList").html(self.template({dishes: dishes}))
+            //TODO@Lian - (Hint: You can look at above method or https://parse.com/docs/js/guide for your reference)
         },
 
         toNewRestaurantPageClick: function() {
