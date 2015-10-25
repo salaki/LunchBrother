9bf01a23e285d39d009c8dee64ea97fe2697b1d2
Ling Hung <ling@xappmedia.com>
2015-10-24 22:52:07 -0400

    MPD-68 Add both dev and prod app keys

--- app/js/main.js
+++ app/js/main.js
@@ -11,12 +11,12 @@ define([
     console.log("main init");
 
       //Dev
-      var appId = 'Cgz1qCbMW85tSsBrYhMlWThKm1pYT5D4U0NmEGxX';
-      var jsKey = 'FAfMIEjgQENLxipP5ddW3YYqu14l9dG9uzGyRSLG';
+      //var appId = 'Cgz1qCbMW85tSsBrYhMlWThKm1pYT5D4U0NmEGxX';
+      //var jsKey = 'FAfMIEjgQENLxipP5ddW3YYqu14l9dG9uzGyRSLG';
 
-      ////Prod
-      //var appId = 'shB8up4c14Idr6eFH4SBjzqZ1vdYT0Q79LSaPQwT';
-      //var jsKey = 'PQrHeggtLnjUfFh4KI1IV5vLhZXztUzfdlUnk5X2';
+      //Prod
+      var appId = 'shB8up4c14Idr6eFH4SBjzqZ1vdYT0Q79LSaPQwT';
+      var jsKey = 'PQrHeggtLnjUfFh4KI1IV5vLhZXztUzfdlUnk5X2';
 
     Parse.initialize(appId, jsKey);
     
