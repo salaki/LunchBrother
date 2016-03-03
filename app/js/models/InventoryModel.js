20299b23ba8f55ada053f9bc47d5db43baf0fb3e
Ling Hung <ling@xappmedia.com>
2016-03-03 13:25:16 -0500

    Add InventoryCollection, InventoyView, InventoryModel, and inventoryTemplate

--- app/js/models/InventoryModel.js
+++ app/js/models/InventoryModel.js
@@ -3,7 +3,23 @@
  */
 define(function() {
     var InventoryModel = Parse.Object.extend("Inventory", {
+        initialize: function() {
+            this.set({count: 0});
+        },
 
+        addOne: function() {
+            this.set({count: this.get('count') + 1});
+        },
+
+        minusOne: function() {
+            if (this.get('count') > 0) {
+                return this.set({ count: this.get('count') - 1 });
+            }
+        },
+
+        charge: function(){
+            return this.get('count') * this.get('Unit_Price');
+        }
     });
     return InventoryModel;
 });
