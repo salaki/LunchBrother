0e20d7a75b55f1e77d70baf207330bcb1f24c47f
Ling Hung <ling@xappmedia.com>
2016-03-03 13:30:48 -0500

    Create some constants

--- app/js/app.js
+++ app/js/app.js
@@ -41,12 +41,70 @@ var config = {
 //Permission code
 var GENERAL_USER = 1, DRIVER = 2, DISTRIBUTOR = 3, LOCAL_MANAGER = 4, LB_ADMIN = 5;
 
-//UMCP grid id
+//Default Grid Id and DP Id
+var DEFAULT_DP = "DBR7M5Pw6q";
 //var UMCP_GRID_ID = 'nmbyDzTp7m'; //dev
 var UMCP_GRID_ID = 'GPOeekfiTI'; //prod
 
 //var STRIPE_KEY = "pk_test_pb95pxk797ZxEFRk55wswMRk"; //dev
 var STRIPE_KEY = "pk_live_YzLQL6HfUiVf8XAxGxWv5AkH"; //prod
+
+//Start Order Time and Stop Order Time
+var START_ORDER_TIME = function() {
+    var current = new Date();
+    var startOrderTime;
+    if (current.getHours() > 14) {
+        startOrderTime = new Date();
+        startOrderTime.setHours(14, 0, 0, 0);
+
+    } else {
+        startOrderTime = new Date(current.getTime() - 24 * 60 * 60 * 1000);
+        startOrderTime.setHours(14, 0, 0, 0);
+    }
+    return startOrderTime;
+};
+
+var STOP_ORDER_TIME = function() {
+    var current = new Date();
+    var stopOrderTime = new Date();
+    if (current.getHours() > 14) {
+        stopOrderTime.setHours(23, 59, 59, 59);
+    } else {
+        stopOrderTime.setHours(10, 30, 0, 0);
+    }
+    return stopOrderTime;
+};
+
+//Inventory Query Times
+var INVENTORY_FROM_TIME = function() {
+    var current = new Date();
+    var fromTime;
+    if (current.getHours() >= 14) {
+        //After 14:00, display the inventory of the next day
+        fromTime = new Date(current.getTime() + 24 * 60 * 60 * 1000);
+        fromTime.setHours(10, 0, 0, 0);
+    }
+    else {
+        //Before 14:00, display the inventory of the current day
+        fromTime = new Date(current.getTime());
+        fromTime.setHours(10, 0, 0, 0);
+    }
+    return fromTime;
+};
+var INVENTORY_UNTIL_TIME = function() {
+    var current = new Date();
+    var untilTime;
+    if (current.getHours() >= 14) {
+        //After 14:00, display the inventory of the next day
+        untilTime = new Date(current.getTime() + 24 * 60 * 60 * 1000);
+        untilTime.setHours(13, 0, 0, 0);
+    } else {
+        //Before 14:00, display the inventory of the current day
+        untilTime = new Date();
+        untilTime.setHours(13, 0, 0, 0);
+    }
+    return untilTime;
+};
 
 require.config(config);
 
